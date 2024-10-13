0018ef472e3045296e1c26529a1929225d9316ba
Irpan Padillah <irpan@argapro.id>
2024-10-13 16:15:04 +0700

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const product = [
+   {
+      id: 1,
+      name: "iPhone 16",
+      price: 100
+   },
+   {
+      id: 2,
+      name: "iPhone 18",
+      price: 200
+   }
+];
+
+
+// console.log(product);
+
+const dataA = () => "Remmote";
+const gradeScore = (score) => (score > 90 ? "Great" : "Good");
+console.log(dataA);
+console.log(gradeScore(90));
+
+
